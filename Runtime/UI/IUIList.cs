a0888fa3f36d7ed027ad9b6ea031727b7962c658
Gyb <helloholden@163.com>
2023-09-28 11:40:05 +0800

    Revert "[modify]CommonList变为泛型"
    This reverts commit dab9566a5db207110bb0adcbf0a0a6a538ee36e6.

--- Runtime/UI/IUIList.cs
+++ Runtime/UI/IUIList.cs
@@ -1,14 +1,14 @@
-﻿//使用utf-8
+//使用utf-8
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace CommonBase
 {
-    public interface IUIList<T>
+    public interface IUIList
     {
         public GameObject ItemPrefab { get; }
-        public void BindData(T[] data);
+        public void BindData<T>(T[] data);
     }
 }
 
