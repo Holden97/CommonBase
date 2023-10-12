fc13b272dee61876e1bcc7df29f4748644bab599
Gyb <479276132@qq.com>
2023-10-12 22:03:17 +0800

    [add]添加转场预设

---
+++ Runtime/UI/ChangeScenePanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+
+namespace CommonBase
+{
+    public class ChangeScenePanel : BaseUI
+    {
+        public Text contentText;
+        public Button confirmBtn;
+        public override void UpdateView(object o)
+        {
+            var content = o as string;
+            contentText.text = content;
+        }
+
+        public override void Initialize()
+        {
+            confirmBtn.onClick.AddListener(() => { this.Close(); });
+        }
+    }
+}
