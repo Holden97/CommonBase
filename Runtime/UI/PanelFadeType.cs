480bb96a28cb9a7e67ce9ea4ae8c4ad16a31e562
Gyb <helloholden@163.com>
2023-10-17 13:58:41 +0800

    [add]添加Button类型

---
+++ Runtime/UI/PanelFadeType.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PanelFadeType 
+{
+    /// <summary>
+    /// 无效果
+    /// </summary>
+    None,
+    /// <summary>
+    /// 中心展开
+    /// </summary>
+    FOLD,
+    /// <summary>
+    /// 向下展开后向右展开
+    /// </summary>
+    DONW_RIGHT,
+}
