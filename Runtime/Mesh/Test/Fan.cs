0c84f2b8a22e0ecade5c41b6625a5f6c8de27803
Gyb <helloholden@163.com>
2023-10-30 11:10:58 +0800

    [add]添加扇形绘制

--- Runtime/Mesh/Test/Fan.cs
+++ Runtime/Mesh/Test/Fan.cs
@@ -1,26 +1,30 @@
-﻿using System.Collections;
+//使用utf-8
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace CommonBase
 {
-    /// <summary>
-    /// 扇形生成器
-    /// </summary>
-    public class Fan : AbstractMeshGenerator
+    public class Fan
     {
-        public float beginAngle;
-        public float endAngle;
+        public Vector3 center;
         public float radius;
+        public float arcDegree;
+        public float centerlineDegree;
 
-        public FanFoldCenter fanFoldType;
-
-
-        public enum FanFoldCenter
+        /// <summary>
+        /// 点是否在扇形内
+        /// 1.点据终点距离小于半径
+        /// 2.点与中点形成向量与扇形中线点乘，所得角度小于1/2的半径
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public bool PointInRange(Vector3 target)
         {
-            LEFT,
-            CENTER,
-            RIGHT,
+            var shorter = Vector3.Distance(target, center) < radius;
+            var dot = Vector3.Dot((target - center).normalized, new Vector3(Mathf.Cos(Mathf.Deg2Rad * arcDegree), Mathf.Sin(Mathf.Deg2Rad * arcDegree)));
+            var lessThanHalfRadius = Mathf.Acos(dot) < radius / 2;
+            return shorter && lessThanHalfRadius;
         }
     }
 }
---
+++ Runtime/Mesh/Test/FanMesh.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonBase
+{
+    /// <summary>
+    /// 扇形生成器
+    /// </summary>
+    public class FanMesh : AbstractMeshGenerator
+    {
+        [Range(0, 360)] public float centerLineDegree;
+        /// <summary>
+        /// 展开弧度
+        /// </summary>
+        [Range(0, 360)] public float arcDegree;
+        [Range(0.1f, 10)] public float radius = 1;
+        private float beginAngle;
+        private float endAngle;
+        public Vector3 centerPoint;
+
+        public FanFoldCenter fanFoldType;
+
+        protected override void SetVertices()
+        {
+            vertices.Add(Vector3.zero);
+
+            vertices.Add(GetVertice(beginAngle));
+
+            float i = beginAngle;
+            for (i = beginAngle + 1; i < endAngle; i++)
+            {
+                vertices.Add(GetVertice(i));
+            }
+
+            vertices.Add(GetVertice(endAngle));
+        }
+
+        protected override void SetMeshNums()
+        {
+            //确保展开弧度为非负数
+            var realArcDegree = Mathf.Abs(arcDegree);
+            if (arcDegree == 0) { realArcDegree = 0.001f; }
+            beginAngle = centerLineDegree - realArcDegree / 2;
+            endAngle = centerLineDegree + realArcDegree / 2;
+
+            var pointsOnCurve = Mathf.Max(Mathf.CeilToInt(realArcDegree + 1), 2);
+            numVertices = pointsOnCurve + 1;
+            numTriangles = (pointsOnCurve - 1) * 3;
+        }
+
+        private Vector3 GetVertice(float degree)
+        {
+            var rad = Mathf.Deg2Rad * degree;
+            return (new Vector3(Mathf.Cos(rad) * radius, Mathf.Sin(rad) * radius, 0));
+        }
+
+        protected override void SetTriangles()
+        {
+            for (int i = 1 + 1; i < vertices.Count; i++)
+            {
+                triangles.Add(0);
+                triangles.Add(i);
+                triangles.Add(i - 1);
+            }
+        }
+
+        public enum FanFoldCenter
+        {
+            LEFT,
+            CENTER,
+            RIGHT,
+        }
+    }
+}
