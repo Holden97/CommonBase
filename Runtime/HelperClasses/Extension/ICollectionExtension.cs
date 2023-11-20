dab02215893e73dac287940da48787cc7cfc3093
Gyb <479276132@qq.com>
2023-11-20 23:31:50 +0800

    1

---
+++ Runtime/HelperClasses/Extension/ICollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonBase
+{
+    public static class ICollectionExtensions
+    {
+        public static bool IsNullOrEmpty(this ICollection list)
+        {
+            return list == null || list.Count == 0;
+        }
+    }
+}
