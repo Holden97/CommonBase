3066e266aeddeb8ff66b677410a0c829cefa2d99
Gyb <479276132@qq.com>
2023-12-24 14:59:57 +0800

    [remove]暂时删除toggle相关内容，放入项目文件中测试

--- Runtime/UI/IListItem.cs
+++ Runtime/UI/IListItem.cs
@@ -1,6 +1,7 @@
-//使用utf-8
+﻿//使用utf-8
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace CommonBase
@@ -16,20 +17,14 @@ namespace CommonBase
         public string ID { get; }
         bool IsToggle { get; set; }
 
-        public Action<IToggle> OnClickToggle { get; set; }
         void OnToggleUnselect();
         void OnToggleSelect();
 
-        public void AddSelectedAction(UnityAction<IToggle> action);
-        public void AddUnselectedAction(UnityAction<IToggle> action);
     }
 
     public interface IToggleGroup
     {
         void SelectToggle(IToggle toggle);
-        public void SetOnSelectedToAll(UnityAction<IToggle> action);
-        public void SetOnUnselectedToAll(UnityAction<IToggle> action);
-
         void Initialize();
         List<IToggle> SetToggles(string tag);
     }
