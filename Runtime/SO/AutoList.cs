d51d5251082ab9489df9664eb9fcc120c0948e51
Gyb <479276132@qq.com>
2024-02-25 21:20:13 +0800

    1

---
+++ Runtime/SO/AutoList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonBase
+{
+    public class AutoListSO<T> : ScriptableObject
+    {
+        public List<T> info;
+    }
+}
