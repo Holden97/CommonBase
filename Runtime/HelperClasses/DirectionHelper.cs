aa692eff293d54aa872a16e0d70a0c3d9d0dc904
Gyb <479276132@qq.com>
2024-03-16 23:30:50 +0800

    1

--- Runtime/HelperClasses/DirectionHelper.cs
+++ Runtime/HelperClasses/DirectionHelper.cs
@@ -2,7 +2,7 @@
 
 namespace CommonBase
 {
-    public enum DirectionEnum { Left, Right, Up, Down,Center }
+    public enum DirectionEnum { Left, Right, Up, Down, Center }
     public class DirectionHelper
     {
         public static Vector2Int JudgeDir(Vector2 center, Vector2 point)
@@ -34,6 +34,17 @@ namespace CommonBase
         {
             var offset = point - center;
             var r = Mathf.Atan2(offset.y, offset.x);
+            var a = r * Mathf.Rad2Deg;
+            if (a < 45 && a >= -45) return DirectionEnum.Right;
+            else if (a < -135 || a >= 135) return DirectionEnum.Left;
+            else if (a >= 45 && a < 135) return DirectionEnum.Up;
+            else if (a < -45 && a >= -135) return DirectionEnum.Down;
+            return DirectionEnum.Center;
+        }
+
+        public static DirectionEnum JudgeDirByDir(Vector2 dir)
+        {
+            var r = Mathf.Atan2(dir.y, dir.x);
             var a = r * Mathf.Rad2Deg;
             if (a < 45 && a >= -45) return DirectionEnum.Right;
             else if (a < -135 || a >= 135) return DirectionEnum.Left;
