69b3621e728621bc6cb2202481c192427e0f2c64
G.Carrot <479276132@qq.com>
2024-04-10 10:16:32 +0800

    评论色

--- Runtime/HelperClasses/Extension/StringExtension.cs
+++ Runtime/HelperClasses/Extension/StringExtension.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using PlasticPipe.PlasticProtocol.Messages;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Plastic.Newtonsoft.Json.Serialization;
 using UnityEngine;
 
 namespace CommonBase
@@ -9,6 +11,42 @@ namespace CommonBase
         public static bool IsNullOrEmpty(this string str)
         {
             return str == null || str.Trim() == "";
+        }
+
+        public static string ToGreen(this string str)
+        {
+            return "<color=#00FF00>" + str + "</color>";
+        }
+
+        public static string ToRed(this string str)
+        {
+            return "<color=#FF0000>" + str + "</color>";
+        }
+
+        public static string ToRemarkColor(this string str, Func<bool> goodFunc, Func<bool> badFunc = null)
+        {
+            if (goodFunc != null && goodFunc())
+            {
+                str = str.ToGreen();
+            }
+            else if (badFunc != null && badFunc())
+            {
+                str = str.ToRed();
+            }
+            return str;
+        }
+
+        public static string ToRemarkValueColor(this string str, float selfValue, float d = 0)
+        {
+            if (selfValue < d)
+            {
+                str = str.ToRed();
+            }
+            else if (selfValue > d)
+            {
+                str = str.ToGreen();
+            }
+            return str;
         }
     }
 }
