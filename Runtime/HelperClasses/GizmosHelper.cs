b0d2517f630f118e045aba410d98938f7b7ee35a
G.Carrot <479276132@qq.com>
2024-07-04 15:57:42 +0800

    1

--- Runtime/HelperClasses/GizmosHelper.cs
+++ Runtime/HelperClasses/GizmosHelper.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace CommonBase
 {
-    public class GizmosHelper
+    public static class GizmosHelper
     {
         /// <summary>
         /// 基于Unity世界坐标绘制矩形
@@ -39,6 +39,47 @@ namespace CommonBase
             }
 #endif
         }
+
+        public static void DrawCircleOnGizmos(this Transform m_Transform, float m_Radius, Color m_Color, float m_Theta = 0.1f)
+        {
+            if (m_Transform == null) return;
+            if (m_Theta < 0.0001f) m_Theta = 0.0001f;
+
+            // 设置矩阵
+            Matrix4x4 defaultMatrix = Gizmos.matrix;
+            Gizmos.matrix = m_Transform.localToWorldMatrix;
+
+            // 设置颜色
+            Color defaultColor = Gizmos.color;
+            Gizmos.color = m_Color;
+
+            // 绘制圆环
+            Vector3 beginPoint = Vector3.zero;
+            Vector3 firstPoint = Vector3.zero;
+            for (float theta = 0; theta < 2 * Mathf.PI; theta += m_Theta)
+            {
+                float x = m_Radius * Mathf.Cos(theta);
+                float z = m_Radius * Mathf.Sin(theta);
+                Vector3 endPoint = new Vector3(x, 0, z);
+                if (theta == 0)
+                {
+                    firstPoint = endPoint;
+                }
+                else
+                {
+                    Gizmos.DrawLine(beginPoint, endPoint);
+                }
+                beginPoint = endPoint;
+            }
+
+            // 绘制最后一条线段
+            Gizmos.DrawLine(firstPoint, beginPoint);
+            // 恢复默认颜色
+            Gizmos.color = defaultColor;
+            // 恢复默认矩阵
+            Gizmos.matrix = defaultMatrix;
+        }
+
     }
 }
 
