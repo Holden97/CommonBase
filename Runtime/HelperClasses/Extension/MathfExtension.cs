7eb8589ca3ef9b83078593ba31b649fcc6401235
Gyb <479276132@qq.com>
2025-02-03 11:59:50 +0800

    1

---
+++ Runtime/HelperClasses/Extension/MathfExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonBase
+{
+    public static class MathfExtension
+    {
+        public static string ToString(this float value, int places)
+        {
+            return value.ToString($"f{places}");
+        }
+
+        public static float ToRound(this float value, int places)
+        {
+            return (Mathf.Round(value * Mathf.Pow(10, places)) / Mathf.Pow(10, places));
+        }
+    }
+}
