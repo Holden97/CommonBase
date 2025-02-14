8570b558177eb67abf844da6ce2ecd29e4761aab
G.Carrot <479276132@qq.com>
2025-02-14 20:26:05 +0800

    1

--- Runtime/SO/SO_UIPath.cs
+++ Runtime/SO/SO_UIPath.cs
@@ -8,7 +8,7 @@ namespace CommonBase
     /// <summary>
     /// 扫描项目中所有的带BaseUI的预制体，
     /// </summary>
-    [CreateAssetMenu(fileName = "SO_UIPath", menuName = "Scriptable Object/UIPath")]
+    [CreateAssetMenu(fileName = "SO_UIPath", menuName = "Scriptable Objects/UIPath")]
     public class SO_UIPath : ScriptableObject
     {
         public List<UIInfo> uIInfos;
